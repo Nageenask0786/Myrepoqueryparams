f713208e90ae1a9c8c9401f07eff90cd8c62516f
Nageena.Shaik <sm8874006@gmail.com>
2023-03-25 06:45:02 +0000

    COMITCODING8A

---
+++ app.js
@@ -0,0 +1,158 @@
+const express = require("express");
+
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "todoApplication.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+const hasPriorityAndStatusProperties = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  );
+};
+
+const hasPriorityProperty = (requestQuery) => {
+  return requestQuery.priority !== undefined;
+};
+
+const hasStatusProperty = (requestQuery) => {
+  return requestQuery.status !== undefined;
+};
+app.get("/todos/", async (request, response) => {
+  let data = null;
+  let getTodosQuery = "";
+  const { search_q = "", priority, status } = request.query;
+
+  switch (true) {
+    case hasPriorityAndStatusProperties(request.query):
+      getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND status = '${status}'
+    AND priority = '${priority}';`;
+      break;
+    case hasPriorityProperty(request.query):
+      getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND priority = '${priority}';`;
+      break;
+    case hasStatusProperty(request.query):
+      getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND status = '${status}';`;
+      break;
+    default:
+      getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%';`;
+  }
+
+  data = await db.all(getTodosQuery);
+  response.send(data);
+});
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoQuery = `SELECT 
+    * FROM todo 
+    WHERE id = ${todoId};`;
+  const Todo = await db.get(getTodoQuery);
+  response.send(Todo);
+});
+////
+app.post("/todos/", async (request, response) => {
+  const todoDetails = request.body;
+  const { todo, priority, status } = todoDetails;
+  const addTodoQuery = `INSERT INTO todo
+    (todo,priority,status)
+    VALUES ('${todo}','${priority}','${status}');`;
+  const dbResponse = await db.run(addTodoQuery);
+  const todoId = dbResponse.lastId;
+  response.send("Todo Successfully Added");
+});
+//API4
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let updateColumn = "";
+  const requestBody = request.body;
+  switch (true) {
+    case requestBody.status !== undefined:
+      updateColumn = "Status";
+      break;
+    case requestBody.priority !== undefined:
+      updateColumn = "Priority";
+      break;
+    case requestBody.todo !== undefined:
+      updateColumn = "Todo";
+      break;
+  }
+  const previousTodoQuery = `
+  SELECT 
+  *
+  FROM
+    todo 
+WHERE 
+  id = ${todoId};`;
+  const previousTodo = await db.get(previousTodoQuery);
+  const {
+    todo = previousTodo.todo,
+    priority = previousTodo.priority,
+    status = previousTodo.status,
+  } = request.body;
+  const UpdateTodoQuery = `UPDATE todo 
+ SET 
+ todo = '${todo}',
+    priority = '${priority}',
+    status = '${status}'
+    WHERE id = ${todoId};`;
+  await db.run(UpdateTodoQuery);
+  response.send(`${updateColumn} Updated`);
+});
+//api5
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `DELETE FROM todo 
+    WHERE id = ${todoId};`;
+  await db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
+});
+module.exports = app;
